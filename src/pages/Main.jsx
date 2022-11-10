2bf6361f781acfdf6c3ef8bcfbef76ae867ab0f2
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-11-10 11:46:40 +0100

    Main page structure added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,28 @@
+import { useContext } from "react";
+import { ContainerContext } from "../App";
+import {
+  MainContainer,
+  MovieSearchPart,
+  SearchButton,
+  SearchInput,
+} from "../styles/MainStyles";
+
 const Main = () => {
-  return <div>MAIN</div>;
+  const { getData, setMovieUserSearchInput } = useContext(ContainerContext);
+  const searchClicked = () => {
+    getData();
+  };
+  return (
+    <MainContainer>
+      <MovieSearchPart>
+        <SearchInput
+          type="text"
+          placeholder="Search a movie"
+          onChange={(event) => setMovieUserSearchInput(event.target.value)}
+        />
+        <SearchButton onClick={searchClicked}>Search</SearchButton>
+      </MovieSearchPart>
+    </MainContainer>
+  );
 };
 export default Main;
